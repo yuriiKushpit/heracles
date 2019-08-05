c5ed528617f430c20ee7cc117a78d10b56fbe346
ykushpit <yurii.kushpit@globallogic.com>
2019-08-05 17:09:18 +0300

    Finished simple validation

--- money_formatting.py
+++ money_formatting.py
@@ -1,17 +1,22 @@
+import locale
+from decimal import Decimal, ROUND_HALF_UP
+
+locale.setlocale(locale.LC_ALL, '')
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/",methods = ['GET'])
+@app.route("/", methods=['GET'])
 def main():
     return render_template("index.html")
 
 
-@app.route('/format',methods = ['POST', 'GET'])
+@app.route('/format', methods=['POST', 'GET'])
 def format():
-    format = request.form['data']
-    return render_template("format.html",formatted_value = format)
+    format = '{:n}'.format(
+        Decimal(request.form['data']).quantize(Decimal('.01'), rounding=ROUND_HALF_UP))
+    return render_template("format.html", formatted_value=format.replace(',','.'))
 
 
 if __name__ == "__main__":
